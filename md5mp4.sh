439bb559922778a069572a4b45eb2987806a61fa
Gerard Benito (ENTI) <gerard.benito@enti.cat>
2020-11-30 12:05:03 +0100

    FFmpeg y esas cosas

--- md5mp4.sh
+++ md5mp4.sh
@@ -4,9 +4,11 @@ echo "Ejercicio de bucles y MD5"
 
 for VARIABLE in `ls *.mp4`; do
 	echo "-----------------------------"
-	VIDEO=`file $VARIABLE | grep Media`
+	VIDEO=`file $VARIABLE | grep -i media`
 	echo $VIDEO
 	if [ "$VIDEO" != "" ]; then
-		md5sum $VARIABLE
+		#md5sum $VARIABLE
+		NOMBRE=`echo $VARIABLE | cut -d "." -f 1`
+		ffmpeg -i $VARIABLE $NOMBRE.mkv	
 	fi
 done
